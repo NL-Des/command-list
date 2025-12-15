c41f26dd2537dae88f8ab0ee91ae2b2eded2e796
nleonard <nathan33.travail@gmail.com>
2025-12-15 11:05:21 +0100

    ajout de la commande pour docker qui autorise les utilisateurs à utiliser librement docker.

--- scriptNewComputer.bash
+++ scriptNewComputer.bash
@@ -126,6 +126,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER #Pour obtenir le droit d'utiliser les commande sans utiliser tout le temps sudo et devoir rentre notre mot de passe.
 
 # Pour afficher les caractéristiques de l'ordinateur.
 neofetch
